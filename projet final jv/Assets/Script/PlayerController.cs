44d935842e6fc36d34c7bd628570af22e62a1796
2031253 <129754686+2031253@users.noreply.github.com>
2023-11-16 15:12:08 -0500

    Animation et gestion arme

---
+++ projet final jv/Assets/Script/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject arme;
+    public bool canShoot = true;
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arme.SetActive(false);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //s'il a l'arme , onle desactive , s'il ne l'a pas , on l'active
+        if (canShoot && Input.GetKeyDown(KeyCode.Space))
+        {
+            canShoot = false;
+            animator.SetTrigger("shoot_trig");
+            arme.SetActive(true);
+            Invoke(nameof(HideGun), 1f);
+     
+        }
+    }
+
+    public void HideGun() {
+        arme.SetActive(false);
+        canShoot = true;
+    }
+
+}
